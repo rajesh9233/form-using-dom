44a757822dbff2becfba3f896972e96eab5b376b
Rajesh <rajeshmurugesan228@gmail.com>
2021-07-02 17:56:52 +0530

    latest changes

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
+'use strict';
+var forms = document.getElementsByClassName('needs-validation');
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        console.log(form.checkValidity())
+        if (form.checkValidity() === false) {
+        event.stopPropagation();
+        }else{
+            console.log(forms);
+            iterateTableData();
+        }
+        form.classList.add('was-validated');
+    }, false);
+    });
 
+  
 var myForm = document.getElementById('myForm');
 let formValues = [];
-myForm.addEventListener('submit',(event)=>{
-    event.preventDefault();
+
+function iterateTableData() {
+    var cboxes = document.getElementsByName('food[]');
+    var len = cboxes.length;
+    let selectedFood = [];
+    for (var i=0; i<len; i++) {
+        if(cboxes[i].checked){
+            selectedFood.push(cboxes[i].value);
+        }
+    }
     formValues.push({
         firstName: myForm.firstName.value,
         lastName: myForm.lastName.value,
         address: myForm.address.value,
         pincode: myForm.pincode.value,
         gender: myForm.gender.value,
-        food: myForm.food.value,
+        food: selectedFood,
         country: myForm.country.value,
         state: myForm.state.value
     })
-    iterateTableData(formValues);
-    myForm.reset();
-})
-
-function iterateTableData(values) {
     var tbody = '';
-    values.forEach(value=>{
+    formValues.forEach(value=>{
         tbody += "<tr>";
         tbody += `<td> ${value.firstName} </td>`;
         tbody += `<td> ${value.lastName} </td>`;
@@ -32,4 +53,6 @@ function iterateTableData(values) {
     });
     var tableData = document.getElementById('table-data');
     tableData.innerHTML = tbody;
+    myForm.reset();
+    forms.classList.remove('was-validated');
 }
